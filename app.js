23c8fd3330ed9be91b6febbfccbfc688a9d0e282
David Boland <dboland77@gmail.com>
2020-12-08 15:52:39 +0000

    Added the ability to query the endpoint and parse json parameters

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const Qs = require('qs'); //querystring parse/stringify
 
+// Add the ability to query our endpoint
+const server = Hapi.server({
+  host: "localhost",
+  port: 4000,
+  query: {
+    parser: (query) => Qs.parse(query)
+  }
+});
+
+// Add a GET route 
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request,h) => {
+    return request.query;
+  }
+})
+
+
+// Example query (per docs)
+// http://localhost:4000/?foo[bar]=baz
+// Command line or postman
 const init = async () => {
   // Set the connection parameters
   // This is not going to be deployed so I am
   // setting to run on localhost 4000 (React likes 3000)
-  const server = Hapi.server({
-    host: "localhost",
-    port: 4000,
-  });
 
-
-  // Add a GET route 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request,h) => {
-      return "Hello World!";
-    }
-  })
   await server.start();
   console.log(`Server running on ${server.info.uri}`)
 };
